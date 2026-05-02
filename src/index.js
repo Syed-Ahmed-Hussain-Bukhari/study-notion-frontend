45a7623158d4abc287fedda1be38d75d0e85f031
Syed-Ahmed-Hussain-Bukhari <146479882+Syed-Ahmed-Hussain-Bukhari@users.noreply.github.com>
2026-05-02 16:22:58 -0700

    fix basename and spa redirect

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/studynotion-frontend">
+      <BrowserRouter basename="/study-notion-frontend">
         <App />
         <Toaster />
       </BrowserRouter>
